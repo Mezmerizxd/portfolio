ec371b1ed6446c250cd94f200062b276f5f852b3
SpetsnazSicario <rorypearson2003@hotmail.com>
2022-06-18 18:35:46 +0100

    push

--- src/Portfolio/index.tsx
+++ src/Portfolio/index.tsx
@@ -7,7 +7,6 @@ import Home from './Home';
 const Portfolio = () => {
     return (
         <div className="Application-container">
-            <h1>Test</h1>
             <Home />
         </div>
     );
