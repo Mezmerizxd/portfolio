fff94614a45edf107b599f2f45cf2f925c1c18eb
SpetsnazSicario <rorypearson2003@hotmail.com>
2022-06-18 18:24:31 +0100

    Update index.tsx

--- src/Portfolio/index.tsx
+++ src/Portfolio/index.tsx
@@ -7,6 +7,7 @@ import Home from './Home';
 const Portfolio = () => {
     return (
         <div className="Application-container">
+            <h1>Test</h1>
             <Home />
         </div>
     );
