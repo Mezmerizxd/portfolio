f7fac62cef6744b1bb7ceb6af681e5ae71a8fa2d
SpetsnazSicario <rorypearson2003@hotmail.com>
2022-08-05 17:22:14 +0100

    content update

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -3,13 +3,26 @@ import './styles.scss';
 interface SectionProps {
     title: string;
     content: string;
+    linkName?: string;
+    link?: string;
 }
 
-const Section = ({ title, content }: SectionProps) => {
+const Section = ({ title, content, linkName, link }: SectionProps) => {
     return (
         <div className="Section">
             <h1 className="Title">{title}</h1>
             <p className="Content">{content}</p>
+            {link && (
+                <div className="Button-container">
+                    <button
+                        onClick={() => {
+                            window.open(link, '_blank');
+                        }}
+                    >
+                        {linkName}
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
